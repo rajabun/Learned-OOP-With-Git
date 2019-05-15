a78689c46dd24d6c0b41881aa0d00ba3fd22e2be
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-05-15 12:31:40 +0700

    Tambah Comment Baru

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-//tambah comment baru
+
+    //tambah comment baru
 
 }
 
