0c2a37c7e2adf15c270787aebee268fd441b851a
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-05-15 15:18:16 +0700

    Passes Hands On CD10 & CD17 Learning Objective

---
+++ LearnerOOP/LearnerHandsOn.swift
@@ -0,0 +1,28 @@
+//
+//  LearnerHandsOn.swift
+//  LearnerOOP
+//
+//  Created by Muhammad Rajab Priharsanto on 15/05/19.
+//  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
+//
+
+import Foundation
+
+class LearnerHandsOn {
+    var snare: String
+    let wood: String
+    let tuning: String
+    let brand: String
+    
+    init(snareType: String, woodType: String, tuningType: String, brandName: String) {
+        self.snare = snareType
+        self.wood = woodType
+        self.tuning = tuningType
+        self.brand = brandName
+    }
+    
+    func changeToSnare() {
+        self.snare = "Steel"
+    }
+    
+}
--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -13,29 +13,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var learnerNameLabel: UILabel!
     @IBOutlet weak var learnerAgeLabel: UILabel!
     @IBOutlet weak var learnerGenderLabel: UILabel!
+    @IBOutlet weak var learnerSnareLabel: UILabel!
+    @IBOutlet weak var learnerWoodLabel: UILabel!
+    @IBOutlet weak var learnerTuningLabel: UILabel!
+    @IBOutlet weak var learnerBrandLabel: UILabel!
     
-    var learnerInstance: LearnerModel? //You just create a copy of your LearnerModel
+    //var learnerInstance: LearnerModel? //You just create a copy of your LearnerModel
+    
+    var learnerInstance: LearnerHandsOn? //You just create a copy of your LearnerHandsOn
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        /*
         learnerInstance = LearnerModel(nameLearner: "Rajab", ageLearner: 24, genderLearner: "Male", imageProfileLearner: "")
+        */
+        learnerInstance = LearnerHandsOn(snareType: "Nylon", woodType: "Mahogany", tuningType: "Standard", brandName: "Something")
         updateUI()
     }
     
     func updateUI() {
+        /*
         if let instance = learnerInstance {
             learnerNameLabel.text = instance.name
             learnerAgeLabel.text = " \(instance.age)"
             learnerGenderLabel.text = instance.gender
+         */
+        
+        if let instance = learnerInstance {
+            learnerSnareLabel.text = instance.snare
+            learnerWoodLabel.text = instance.wood
+            learnerTuningLabel.text = instance.tuning
+            learnerBrandLabel.text = instance.brand
             
         }
     
     }
     
     @IBAction func increaseAgeButtonClicked(_ sender: UIButton) {
+        /*
         if let instance = learnerInstance {
             instance.increaseAge()
+         */
+        if let instance = learnerInstance {
+            instance.changeToSnare()
             updateUI()
         }
         
