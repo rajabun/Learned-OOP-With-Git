16a3d6e2193f3885f899d8c6f248abf138340c44
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-05-15 15:59:40 +0700

    Create Another Button

---
+++ LearnerOOP/RoundedButton.swift
@@ -0,0 +1,19 @@
+//
+//  RoundedButton.swift
+//  LearnerOOP
+//
+//  Created by Muhammad Rajab Priharsanto on 15/05/19.
+//  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.borderWidth = 1
+        layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        layer.cornerRadius = 5
+    }
+
+}
